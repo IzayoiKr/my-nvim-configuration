6cfde629cdf46ca075892205df39277eb0abcec4
IzayoiKr <izayoilv@gmail.com>
2026-02-26 18:10:38 +0700

    Update switch windows feature with ease

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -6,6 +6,36 @@ return {
         { "<A-j>", "<cmd>SmartResizeDown<cr>",  desc = "Resize down" },
         { "<A-k>", "<cmd>SmartResizeUp<cr>",    desc = "Resize up" },
         { "<A-l>", "<cmd>SmartResizeRight<cr>", desc = "Resize right" },
+
+        -- Moving between splits
+        {
+            "<C-h>",
+            function()
+                require("smart-splits").move_cursor_left()
+            end,
+            desc = "Go to left window",
+        },
+        {
+            "<C-j>",
+            function()
+                require("smart-splits").move_cursor_down()
+            end,
+            desc = "Go to below window",
+        },
+        {
+            "<C-k>",
+            function()
+                require("smart-splits").move_cursor_up()
+            end,
+            desc = "Go to above window",
+        },
+        {
+            "<C-l>",
+            function()
+                require("smart-splits").move_cursor_right()
+            end,
+            desc = "Go to right window",
+        },
     },
     opts = {
         ignored_filetypes = { "nofile", "prompt", "qf" },
